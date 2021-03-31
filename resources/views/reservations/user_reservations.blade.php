8b6806a8f5b5b995749a2fe6c0145a1eb52f6f64
Harry <46583043+HarryKing87@users.noreply.github.com>
2021-03-31 14:21:58 +0300

    Added confetti after successful reservation and proper styling with Confetti.js

--- resources/views/reservations/user_reservations.blade.php
+++ resources/views/reservations/user_reservations.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <h1 class="font-semibold text-xl text-gray-800 leading-tight" >Reservations</h1></br>
+            <h1 class="font-semibold text-xl text-gray-800 leading-tight">Reservations</h1></br>
            
           
 
@@ -28,12 +28,14 @@
                         <h3  class="font-semibold text-xl ">Time</h3>
                         <p>{{ $time }}</p>
                     </div>
+
+                    
                 </div></br>
                 @endforeach  
                 @else
                     <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg d-flex console_info_wr p-10">
                         <div class="console_info">
-                            <h3 class="font-semibold text-xl">You have not any reservations yet! :-)</h3>
+                            <h3 class="font-semibold text-xl">You don't have any reservations yet! :-)</h3>
                         </div>
                     </div>
             @endif
