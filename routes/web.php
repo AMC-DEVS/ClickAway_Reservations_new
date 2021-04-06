0071734c0176e9d320621557ab1cef240efb2a37
design4uMTDEV <design4u.dev@gmail.com>
2021-04-06 17:32:41 +0300

    Fixxed issue with redirections in auth Middleware.

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\CompanyProfileController;
 // Welcome page
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 
 // User authentication
